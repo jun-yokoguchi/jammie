f708bb2835360712827b486bab9782380e152ffd
jun_yokoguchi <jy.ilcy@gmail.com>
2016-05-04 08:24:30 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'techacademy-jun-yokoguchi.c9users.io' }# 認証メールのリンク先が cloud9のアプリケーションになるようにする
+  config.action_mailer.delivery_method = :letter_opener
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'users/new'
+
   root to: 'top_pages#home'
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
  
 end
